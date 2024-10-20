e7cfceececae270120b287ba3cc7eec605da06a6
Ricardo de Camargo <139239844+RicardoCamargoPS@users.noreply.github.com>
2024-10-19 22:59:09 -0300

    criado properties e carga inicial de dados

--- src/main/java/com/brasoft/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/brasoft/helpdesk/HelpdeskApplication.java
@@ -1,13 +1,49 @@
 package com.brasoft.helpdesk;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.brasoft.helpdesk.domain.Chamado;
+import com.brasoft.helpdesk.domain.Cliente;
+import com.brasoft.helpdesk.domain.Tecnico;
+import com.brasoft.helpdesk.enums.Perfil;
+import com.brasoft.helpdesk.enums.Prioridade;
+import com.brasoft.helpdesk.enums.Status;
+import com.brasoft.helpdesk.repositories.ChamadoRepository;
+import com.brasoft.helpdesk.repositories.ClienteRepository;
+import com.brasoft.helpdesk.repositories.TecnicoRepository;
+
 @SpringBootApplication
-public class HelpdeskApplication {
+public class HelpdeskApplication implements CommandLineRunner{
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Tecnico tec1 = new Tecnico(null, "Valdir Cezar", "123456", "valdir@mail.com", "123");
+		tec1.addPerfil(Perfil.TECNICO);
+		Cliente cli1 = new Cliente(null, "Leonardo Silva", "654321", "leonardo@mail.com", "123");
+		cli1.addPerfil(Perfil.CLIENTE);
+		
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+		
 	}
 
 }
--- src/main/java/com/brasoft/helpdesk/domain/Chamado.java
+++ src/main/java/com/brasoft/helpdesk/domain/Chamado.java
@@ -4,8 +4,6 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
-import org.hibernate.annotations.ManyToAny;
-
 import com.brasoft.helpdesk.enums.Prioridade;
 import com.brasoft.helpdesk.enums.Status;
 import com.fasterxml.jackson.annotation.JsonFormat;
